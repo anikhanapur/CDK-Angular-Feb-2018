2f895c8864290807f1d76c8f874db1330a29cb4c
Magesh <tkmagesh77@gmail.com>
2018-02-10 10:01:29 +0530

    introducing IBug and BugOperationsService

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,31 +1,32 @@
 import { Component } from '@angular/core';
-
- 
+import { IBug } from './models/IBug';
+import { BugOperationsService } from './services/bugOperations.service';
 
 @Component({
 	selector : 'app-bug-tracker',
 	templateUrl : 'bugTracker.component.html'
 })
 export class BugTrackerComponent{
-	bugs  =  [];
+	bugs : IBug[] =  [];
 
-	onCreateNewClick(bugName){
-		let newBug = {
-			name : bugName,
-			isClosed : false
-		};
+	constructor(private bugOperations : BugOperationsService){
+		
+	}
+
+	onCreateNewClick(bugName : string) : void {
+		let newBug : IBug = this.bugOperations.createNew(bugName);
 		this.bugs.push(newBug);
 	}
 
-	onBugClick(bug){
-		bug.isClosed = !bug.isClosed;
+	onBugClick(bug : IBug) : void {
+		this.bugOperations.toggle(bug);
 	}
 
-	onRemoveClosedClick(){
+	onRemoveClosedClick() : void {
 		this.bugs = this.bugs.filter(bug => !bug.isClosed);
 	}
 
-	getClosedCount(){
+	getClosedCount() : number {
 		return this.bugs.reduce((result, bug) => bug.isClosed ? ++result : result, 0);
 	}
 }
